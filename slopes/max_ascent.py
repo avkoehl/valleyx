6f8f3285b9fe37ba3dbc726873e4924716f63511
Arthur Koehl <avkoehl@ucdavis.edu>
2024-10-09 17:19:22 -0700

    scaffolding

---
+++ slopes/max_ascent.py
@@ -0,0 +1,10 @@
+"""
+Trace paths of maximum gradient from the river up the slopes
+
+
+given stream ID, sample points along stream, for each stream point generate path (s?) 
+
+breakpoint detection on those paths
+"""
+
+
---
+++ slopes/valley_walls.py
@@ -0,0 +1,32 @@
+"""
+given cross section profile
+segment the cross section by slope mean shift or curvature method
+return breakpoints if any 
+"""
+
+# given cross section:
+# preprocessing
+# reset zero on the channel/thalweg
+#
+
+# CASES:
+# 1. no valley wall in cross section
+# 2. no valley floor
+# 3. valley floor
+#      terraces, alluvial fans, talus, colluvial deposits toes, pediment
+#      levees, embankments, road cuts, islands...
+
+# split into meanshift regions
+#
+#
+#
+# preprocess profile
+# run mean shift
+# if channel not in low slope mean shift
+# use curvature
+# if channel next to a peak curvature - thats the valley floor (points immediate left and right
+#
+# if channel in low mean shift
+# check for terraces
+# 
+
