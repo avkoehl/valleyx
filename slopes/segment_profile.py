d6ec19956a72b8f1871040a3e887e559740720b1
Arthur Koehl <avkoehl@ucdavis.edu>
2024-10-11 15:00:06 -0700

    brainstorm

--- slopes/segment_profile.py
+++ slopes/segment_profile.py
@@ -2,7 +2,7 @@
 Methods to segment a cross section profile into regions by finding breakpoints
 
 These regions may be:
-    - mean shift change point detection on slope
+    - mean shift change point detection on slope using PELT
     - peaks of profile curvature
     - simplification of the elevation profile using Ramer-Douglas-Peucker algorithm
 
@@ -10,19 +10,35 @@ Then we apply a heuristic to classify these regions (find the breakpoint that is
 in this step we may want to do some preprocessing, e.g. if elevation decreases stop the series
 
 """
-from sklearn.cluster import MeanShift
+import numpy as np
+import ruptures as rpt
 
-def mean_shift(series):
-    method = MeanShift()
-    method.fit(series.values.reshape(-1,1))
-    return method.labels_
+def mean_shift(series, model='l2', pen=5):
+    method = rpt.Pelt(model=model).fit(series)
+    result = method.predict(pen=pen)
+    labels = label_segments(series, bps)
+    return labels
+
+
+def label_segments(series, bps):
+    labels = np.zeros(len(series), dtype=int)
+    for i in range(len(bps) -1):
+        start = bps[i]
+        end = bps[i+1]
+        labels[start:end] = i
+    return labels
+
 
 def profile_curvature_peaks(series, height):
     peak_positions = signal.find_peaks(series, height)[0]
-    pass
+    labels = label_segments(series, peak_positions)
+    return labels
 
 def elevation_simplifcation(series):
     # convert series to points (alpha, elevation) to linetring
-    #linestring.simpligy
+    #linestring.simplify
+    pass
+
+def plot_segments(series, labels):
     pass
     
