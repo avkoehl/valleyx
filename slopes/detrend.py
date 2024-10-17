2c39065ac113c871f9ed737e4065a95f74973fc4
Arthur Koehl <avkoehl@ucdavis.edu>
2024-10-17 14:29:41 -0700

    fix mistake

--- slopes/detrend.py
+++ slopes/detrend.py
@@ -1,7 +1,6 @@
 import os
-import sys
 
-import numba
+from numba import njit
 import numpy as np
 import rioxarray as rxr
 import xarray as xr
@@ -9,157 +8,140 @@ from whitebox import WhiteboxTools
 from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import dijkstra
 from pysheds.grid import Grid
-from pysheds.view import Raster 
+from pysheds.view import Raster
 from pysheds.view import ViewFinder
 from pysheds._sgrid import _angle_to_d8_numba
 from pysheds._sgrid import _dinf_hand_iter_numba
-from pysheds._sgrid import _assign_hand_heights_numba 
+from pysheds._sgrid import _assign_hand_heights_numba
 from pysheds._sgrid import _mfd_hand_iter_numba
 
-from valleyfloor.utils import setup_wbt
 
-
-def detrend(dem, flow_paths, wbt, method="hand_steepest"):
+def hand(dem, flow_paths, wbt=None, method="d8") -> xr.DataArray:
     """
-    Detrend a digital elevation model by height above nearest drainage
-    dem should be hydrologically conditioned for steepest, dinf, mfd, or cost
-    
+    Compute elevation above nearest stream cell (aka HAND)
+
     Parameters
     ----------
     dem: xr.DataArray
+        A raster of elevation values. For d8, dinf, mfd, should be a
+        hydrologically conditioned
     flow_paths: xr.DataArray
-    wbt: WhiteboxTools
+        A raster of flow paths where cells > 0 are stream
+    wbt: optional WhiteboxTools
+        An instance of WhiteboxTools class, needed for d8
     method: str
-        'HAND_steepest' as elevation above nearest stream wbt max flow_dir
-        'HAND_euclidean' as elevation above nearest stream based just on euclidean distance
-        'HAND_dinf' as elevation above nearest stream dinfinity flow dir
-        'HAND_multi' as elevation above nearest stream mfd flow dir
-        'HAND_cost' as cost accumulation accumulated change in elevation 
-    
-    Returns
-    -------
-    tuple(xr.DataArray, xr.DataArray) - hand and slope of hand
-    """
-    method_list = ["hand_steepest", "hand_euclidean", "hand_dinf", "hand_multi", "hand_cost"]
-    if method not in method_list:
-        sys.exit(f"choose valid method from {method_list}")
-
-    if method == "hand_steepest":
-        hand = hand_steepest(dem, flow_paths, wbt)
-    elif method == "hand_euclidean":
-        raise NotImplementedError("have not thought this one through")
-        hand = hand_euclidean(dem, flow_paths, wbt)
-    elif method == "hand_dinf":
-        hand = hand_pysheds(dem, flow_paths, routing_method='dinf')
-    elif method == "hand_multi":
-        hand = hand_pysheds(dem, flow_paths, routing_method='mfd')
-    elif method == "hand_cost":
-        graph = construct_cost_graph(dem)
-        hand = hand_cost(dem, flow_paths, graph)
-
-    slope_of_hand = wbt_slope(hand, wbt)
-    return (hand, slope_of_hand)
-
-def hand_steepest(dem: xr.DataArray, flow_paths: xr.DataArray, wbt: WhiteboxTools) -> xr.DataArray:
-    """ 
-    Wrapper around elevation above nearest stream WBT method.
-
-    Parameters
-    ----------
-
-    dem: xr.DataArray
-        A dem that has been hydrologically conditioned
-    flow_paths: xr.DataArray
-        A raster where nonzero values are flow paths that represent the stream path
-    wbt: WhiteboxTools
-		An instance of the whitebox tools class
-    """
-    work_dir = wbt.work_dir
-    names = ['temp_conditioned_dem', 'temp_flowpaths', 'hand']
-    fnames = [os.path.join(work_dir, name + '.tif') for name in names]
-    files = {name:file for name,file in zip(names,fnames)}
-
-    # save conditioned and flowpaths to temp files
-    dem.rio.to_raster(files['temp_conditioned_dem'])
-    flow_paths.rio.to_raster(files['temp_flowpaths'])
-
-    wbt.elevation_above_stream(
-            files['temp_conditioned_dem'],
-            files['temp_flowpaths'],
-            files['hand'])
-
-    with rxr.open_rasterio(files['hand'], masked=True) as raster:
-        hand = raster.squeeze() 
-
-    os.remove(files['temp_conditioned_dem'])
-    os.remove(files['temp_flowpaths'])
-    return hand
-
-def hand_euclidean(dem: xr.DataArray, flow_paths: xr.DataArray, wbt: WhiteboxTools) -> xr.DataArray:
-    """ 
-    Wrapper around elevation above nearest stream euclidean WBT method.
-
-    Parameters
-    ----------
-
-    dem: xr.DataArray
-        A dem that has been hydrologically conditioned
-    flow_paths: xr.DataArray
-        A raster where nonzero values are flow paths that represent the stream path
-    wbt: WhiteboxTools
-		An instance of the whitebox tools class
+        'd8' wrapper around whiteboxtools elevation above stream method
+        'dinf' wrapper around pysheds hand dinf flow routing method
+        'mfd' wrapper around pysheds hand mfd flow routing method
+        'lcp' least cost path route to nearest stream cell
 
     Returns
     -------
     xr.DataArray
-        A dem of elevation above stream
-
+        A raster of elevation above nearest stream values
     """
-    files = {'dem': os.path.join(wbt.work_dir, 'temp_dem.tif'),
-             'flowpaths': os.path.join(wbt.work_dir, 'temp_flowpaths.tif'),
-             'hand': os.path.join(wbt.work_dir, 'hand_e.tif')}
+    methods = ["d8, dinf, mfd", "lcp"]
+    if method == "d8":
+        if wbt is None:
+            raise ValueError("wbt must be provided for d8 method")
+        hand = hand_steepest(dem, flow_paths, wbt)
+    elif method == "dinf":
+        hand = hand_pysheds(dem, flow_paths, routing_method="dinf")
+    elif method == "mfd":
+        hand = hand_pysheds(dem, flow_paths, routing_method="mfd")
+    elif method == "lcp":
+        graph = construct_cost_graph(dem)
+        hand = hand_cost(dem, flow_paths, graph)
+    else:
+        raise ValueError(f"method needs to be one of {methods}")
 
-    dem.rio.to_raster(files['dem'])
-
-    # see https://github.com/jblindsay/whitebox-tools/issues/245 
-    (flow_paths > 1).astype(np.uint8).rio.to_raster(files['flowpaths'])
-    #flow_paths.rio.to_raster(files['flowpaths'])
-
-    wbt.elevation_above_stream_euclidean(
-            files['dem'],
-            files['flowpaths'],
-            files['hand'])
-
-    with rxr.open_rasterio(files['hand'], masked=True) as raster:
-        hand = raster.squeeze() 
-
-    os.remove(files['dem'])
-    os.remove(files['flowpaths'])
     return hand
 
-def hand_pysheds(dem, flow_paths, routing_method):
+
+def hand_steepest(
+    dem: xr.DataArray, flow_paths: xr.DataArray, wbt: WhiteboxTools
+) -> xr.DataArray:
     """
-    conditioned dem
-    compute elevation above nearest stream using dinf flow direction
-    # use pysheds
+    Wrapper around elevation above nearest stream WBT method.
+
+    Parameters
+    ----------
+    dem: xr.DataArray
+        A dem that has been hydrologically conditioned
+    flow_paths: xr.DataArray
+        A raster where nonzero values are flow paths that represent the stream
+        path
+    wbt: WhiteboxTools
+                An instance of the whitebox tools class
+
+    Returns
+    -------
+    xr.DataArray
+        A raster of elevation above stream values
     """
+    work_dir = wbt.work_dir
+    names = ["temp_conditioned_dem", "temp_flowpaths", "hand"]
+    fnames = [os.path.join(work_dir, name + ".tif") for name in names]
+    files = {name: file for name, file in zip(names, fnames)}
+
+    # save conditioned and flowpaths to temp files
+    dem.rio.to_raster(files["temp_conditioned_dem"])
+    flow_paths.rio.to_raster(files["temp_flowpaths"])
+
+    wbt.elevation_above_stream(
+        files["temp_conditioned_dem"], files["temp_flowpaths"], files["hand"]
+    )
+
+    os.remove(files["temp_conditioned_dem"])
+    os.remove(files["temp_flowpaths"])
+
+    try: 
+        hand = rxr.open_rasterio(files["hand"], masked=True).squeeze()
+    except:
+        raise ValueError("WhiteboxTools failed to compute HAND")
+
+    return hand
+
+def hand_pysheds(
+    dem: xr.DataArray, flow_paths: xr.DataArray, routing_method: str
+) -> xr.DataArray:
+    """
+    Parameters
+    ----------
+    dem: xr.DataArray
+        A hydrologically conditioned DEM
+    flow_paths: xr.DataArray
+        A raster where nonzero values are flow paths that represent the stream
+        path
+    routing_method: str
+        'dinf' or 'mfd'
+
+    Returns
+    -------
+    xr.DataArray
+        A raster of elevation above stream values
+    """
+
     def xarray_to_pysheds(raster: xr.DataArray) -> Raster:
-        view = ViewFinder(affine=raster.rio.transform(), shape=raster.shape, crs=raster.rio.crs, nodata=raster.rio.nodata)
+        view = ViewFinder(
+            affine=raster.rio.transform(),
+            shape=raster.shape,
+            crs=raster.rio.crs,
+            nodata=raster.rio.nodata,
+        )
         return Raster(raster.data, viewfinder=view)
 
     def pysheds_to_rioxarray(raster: Raster, grid: Grid) -> xr.DataArray:
         da = xr.DataArray(
-                data = raster,
-                dims = ['y','x'],
-                coords = {
-                    "y": np.linspace(
-                        grid.bbox[3], grid.bbox[1], raster.shape[0]),
-                    "x": np.linspace(
-                        grid.bbox[0], grid.bbox[2], raster.shape[1])
-                    }
-                )
+            data=raster,
+            dims=["y", "x"],
+            coords={
+                "y": np.linspace(grid.bbox[3], grid.bbox[1], raster.shape[0]),
+                "x": np.linspace(grid.bbox[0], grid.bbox[2], raster.shape[1]),
+            },
+        )
         da.rio.write_transform(grid.affine, inplace=True)
-        da.rio.write_crs(grid.crs.to_string(), inplace=True)
+        da.rio.write_crs(grid.crs, inplace=True)
         da.rio.write_nodata(grid.nodata, inplace=True)
         return da
 
@@ -169,31 +151,29 @@ def hand_pysheds(dem, flow_paths, routing_method):
     mask = pysheds_flowpaths > 1
 
     # let pysheds compute flowdir based on routing method
-    if routing_method == 'dinf':
-        fdir = grid.flowdir(pysheds_dem, routing='dinf', nodata_out=np.float64(-1))
+    if routing_method == "dinf":
+        fdir = grid.flowdir(pysheds_dem, routing="dinf", nodata_out=np.float64(-1))
         dirmap = (64, 128, 1, 2, 4, 8, 16, 32)
         nodata_cells = grid._get_nodata_cells(fdir)
 
-        fdir_0, fdir_1, prop_0, prop_1 = _angle_to_d8_numba(fdir, (64,128,1,2,4,8,16,32), nodata_cells)
-        dirleft_0, dirright_0, dirtop_0, dirbottom_0 = grid._pop_rim(fdir_0,nodata=0)
-        dirleft_1, dirright_1, dirtop_1, dirbottom_1 = grid._pop_rim(fdir_1,nodata=0)
-        maskleft, maskright, masktop, maskbottom = grid._pop_rim(mask, nodata=False)
+        fdir_0, fdir_1, prop_0, prop_1 = _angle_to_d8_numba(
+            fdir, (64, 128, 1, 2, 4, 8, 16, 32), nodata_cells
+        )
         hand_idx = _dinf_hand_iter_numba(fdir_0, fdir_1, mask, dirmap)
-    elif routing_method == 'mfd':
-        fdir = grid.flowdir(pysheds_dem, routing='mfd', nodata_out=np.float64(-1))
+    elif routing_method == "mfd":
+        fdir = grid.flowdir(pysheds_dem, routing="mfd", nodata_out=np.float64(-1))
         nodata_cells = grid._get_nodata_cells(fdir)
-        dirleft, dirright, dirtop, dirbottom = grid._pop_rim(fdir, nodata=0.)
-        maskleft, maskright, masktop, maskbottom = grid._pop_rim(mask, nodata=False)
         hand_idx = _mfd_hand_iter_numba(fdir, mask)
     else:
         raise ValueError("routing_method must be 'dinf' or 'mfd'")
-
 
     hand = _assign_hand_heights_numba(hand_idx, pysheds_dem, np.float64(np.nan))
     hand = pysheds_to_rioxarray(hand, grid)
     return hand
 
-def hand_cost(dem: xr.DataArray, flow_paths: xr.DataArray,  graph: csr_matrix) -> xr.DataArray:
+
+def hand_cost(
+    dem: xr.DataArray, flow_paths: xr.DataArray, graph: csr_matrix) -> xr.DataArray:
     """
     compute elevation above nearest stream based on a cost graph. Finds the
     stream point with the lowest cost path for each cell. Then detrends from
@@ -211,16 +191,20 @@ def hand_cost(dem: xr.DataArray, flow_paths: xr.DataArray,  graph: csr_matrix) -
     Returns
     -------
     xr.DataArray
-        A raster of detrended elevations
+        A raster of elevation above stream values
     """
 
     ids = np.arange(flow_paths.size).reshape(flow_paths.shape)
     source_nodes = ids[flow_paths > 0]
 
     # get basins
-    costs, predecessors, basins = dijkstra(graph, directed=True, indices=source_nodes,
-                                           return_predecessors=True,
-                                            min_only=True)
+    _, _, basins = dijkstra(
+        graph,
+        directed=True,
+        indices=source_nodes,
+        return_predecessors=True,
+        min_only=True,
+    )
     basins = basins.reshape(dem.shape)
 
     # get basin elevation values
@@ -232,15 +216,11 @@ def hand_cost(dem: xr.DataArray, flow_paths: xr.DataArray,  graph: csr_matrix) -
         elev = dem.data[row, col]
         stream_elevations.data[basins == cell_id] = elev
 
-    # subtract basin elevation from each cell in dem to get hand
-    hand = dem - stream_elevations + dem.min().item()
+    hand = dem - stream_elevations
     return hand
 
-
-def wbt_slope(dem: xr.DataArray, wbt: WhiteboxTools) -> xr.DataArray:
-    """
-    compute slope of surface, wrapper around wbt
-
+def construct_cost_graph(dem: xr.DataArray):
+	"""
     Parameters
     ----------
     dem: xr.DataArray
@@ -248,52 +228,50 @@ def wbt_slope(dem: xr.DataArray, wbt: WhiteboxTools) -> xr.DataArray:
 
     Returns
     -------
-    xr.DataArray
-        A raster of slope values in degrees
-    """
-    files ={
-            'dem': os.path.join(wbt.work_dir, 'temp_dem.tif'),
-            'slope': os.path.join(wbt.work_dir, 'hslope.tif')}
-    dem.rio.to_raster(files['dem'])
-
-    wbt.slope(files['dem'], files['slope'], units='degrees')
-
-    with rxr.open_rasterio(files['slope'], masked=True) as raster:
-        slope_of_hand = raster.squeeze().copy()
-
-    os.remove(files['dem'])
-    return slope_of_hand
-
-def construct_cost_graph(dem: xr.DataArray) -> csr_matrix:
-    rows, cols, data = _numba_delta_elevation_cost(dem.data) # row is id of source, col is id of target, data is 1
+    csr_matrix
+        A sparse matrix representing the cost graph
+	"""
+    values = dem.data
+    rows, cols, data = _numba_delta_elevation_cost(values)
     graph = csr_matrix((data, (rows, cols)), shape=(dem.size,dem.size))
     return graph
 
-@numba.njit
+@njit
 def _numba_delta_elevation_cost(dem_data: np.ndarray):
+    """
+    Parameters
+    ----------
+    dem_data: np.ndarray
+        An array of elevation values
+
+    Returns
+    -------
+    rows: list
+        A list of row indices
+    cols: list
+        A list of column indices
+    data: list
+        A list of cost values
+
+    """
     nrows, ncols = dem_data.shape
     ids = np.arange(dem_data.size).reshape(dem_data.shape)
-
     rows = []
     cols = []
     data = []
     for r in range(nrows):
         for c in range(ncols):
-
             for dx in [-1, 0, 1]:
                 for dy in [-1, 0, 1]:
                     if dx == 0 and dy == 0:
                         continue
                     nr = r + dy
                     nc = c + dx
-
                     if 0 <= nr < nrows and 0 <= nc <= ncols:
                         cost = np.abs(dem_data[nr, nc] - dem_data[r, c])
-
                         if np.isfinite(cost):
                             if dx != 0 and dy != 0:
                                 cost = cost / 1.41 # diagonal
-
                             rows.append(ids[r,c])
                             cols.append(ids[nr,nc])
                             data.append(cost)
