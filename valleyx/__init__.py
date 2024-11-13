51bedce673ca23689d78631291ba3c11bb328138
Arthur Koehl <avkoehl@ucdavis.edu>
2024-11-13 15:59:44 -0800

    not sure why these were missing

--- valleyx/__init__.py
+++ valleyx/__init__.py
@@ -2,6 +2,6 @@ from .core import extract_valleys
 from .core import ValleyConfig
 from .flow_analysis import flow_analysis
 from .reach_delineation import delineate_reaches
-from .wall_detection import detect_valley_walls
+from .wall_detection import detect_wallpoints
 from .label_floors import label_floors
 from .label_floors import subbasin_floors
