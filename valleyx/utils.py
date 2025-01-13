236d586a85651740ec4fa0897fe97947228f0658
Arthur Koehl <avkoehl@ucdavis.edu>
2025-01-13 15:14:43 -0800

    typo

--- valleyx/utils.py
+++ valleyx/utils.py
@@ -26,6 +26,7 @@ def setup_wbt(working_dir, verbose, max_procs):
     working_dir = os.path.abspath(os.path.expanduser(working_dir))
     if not os.path.exists(working_dir):
         os.makedirs(working_dir)
+    wbt.set_working_dir(working_dir)
 
     wbt.set_verbose_mode(verbose)  # default True
     wbt.set_max_procs(max_procs)  # default -1
